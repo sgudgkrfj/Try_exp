97c6445ed598edee5545851142e85d3306af331d
student <boyvegas955@gmail.com>
2023-05-16 20:45:26 +0300

    fff

--- main.py
+++ main.py
@@ -1,31 +1,71 @@
+#1
+"""class  InvalidFileFormatError(Exception):
+    def __init__(self, username):
+        self.username=username
+
+#InvalidLengthError
+#InvalidCharacterError
+
+
+
+def register_user(username):
+    if len(username)<5:
+        raise InvalidFileFormatError(username)
+    else:
+        print("Вас зарегано! ")
+#приклад використання
 try:
-    print("start code")
-    print(error)
-    print("end code")
-except:
-    print("no problem")
-print("any code...")
+    username=input("Введіть імя користувача: ")
+    register_user(username)
+except InvalidFileFormatError as e:
+    print(f"Неправельне ім'я користувача {e.username} "
+          f"мінімум 5 символів")
 
-"""
-def checker(var_1):
-    if type(var_1)!=str:
-        raise TapeError(f"Сорі, ми не працюємо з {type(var_1)}, ам теба str")
+
+
+
+#2
+class InvalidPasswordError(Exception):
+    def __init__(self, password):
+        self.password=password
+
+def validete_password(password):
+    g=0
+    for i in password:
+        if i.isnumeric():
+            g+=1
+    if g==0 and len(password)<8:
+        raise InvalidPasswordError(password)
     else:
-        return var_1
-checker(1234)
-"""
+        print("Вас зареганоyy!")
 
-class BuildingHouseError(Exception):
-    def __str__(self):
-        return "Щось не те, дуже багато матеріалів"
+try:
+    password=input("Введіть пароль: ")
+    validete_password(password)
+except InvalidPasswordError as r:
+    print(f"Неправильно введений пароль {r.password}")
+
+#3"""
+class InvalidFileFormatError(Exception):
+    def __init__(self,f):
+        self.f=f
+
+def read_file(f):
+    try:
+        with open(f,"r") as file:
+            content = file.read()
+            print("Вміст файлу:", content)
+    except IOError:
+        raise InvalidFileFormatError(f)
+
+try:
+    read_file(input("Введіть назву файлу:"))
+except InvalidFileFormatError as e:
+   print(f"Невірний формат файлу {e.f}")
 
 
-def checker_material_building(amount, limit):
-    if amount > limit:
-        return "Достатньо матеріалів!"
-    else:
-        raise BuildingHouseError()
-checker_material_building(10, 300)
+
+
 
 
 
